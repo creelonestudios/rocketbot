89851c941c7c326a4ab1308c345284c73555a8d3
J0NASLP <jonaslp759@gmail.com>
2021-05-27 13:39:57 +0200

    embed() default values + rocket cmd :) + version

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const token = process.env.TOKEN;
 const prefix = "rocket";
+const version = "0.1.0";
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
@@ -17,8 +18,7 @@ client.on("message", msg => {
 	let channel = msg.channel;
 	if(msg.content.toLowerCase() == "rocket") {
 		// change to embed containing bot description and stuff later
-		channel.send("that's me.");
-		channel.send(embed({title: "Rocket", description: "Rocket to get doge to the moon\n\nSimple bot you can talk to and that helps you.\nTry `rocket help` or `rocket devs`.", footer: {text: "Rocket vINSERt VERSION HERE"}}));
+		channel.send(embed({description: "Rocket to get doge to the moon\n\nSimple bot you can talk to and that helps you.\nTry `rocket help` or `rocket devs`.", author: {name: "Bot Info"}}));
 	} else if(msg.content.toLowerCase().startsWith(prefix+" ")) {
 		let s = msg.content.substring(7);
 		let o = checkCommand(s);
@@ -49,6 +49,15 @@ function checkCommand(s) {
 }
 
 function embed(options) {
+	if(!options.footer) options.footer = {};
+	if(!options.footer.text) options.footer.text = "Rocket v" + version;
+	if(!options.footer.iconURL) options.footer.iconURL = client.user.avatarURL({dynamic: true}); // dynamic gets .gif url if animated
+	if(!options.color) options.color = "#55ACEE";
+	if(!options.url) options.url = "https://github.com/creelonestudios/rocketbot";
+	if(!options.author) options.author = {};
+	if(!options.author.name) options.author.name = "Rocket";
+	if(!options.author.url) options.author.url = "https://github.com/creelonestudios/rocketbot";
+	if(!options.author.iconURL) options.author.iconURL = client.user.avatarURL({dynamic: true}); // dynamic gets .gif url if animated
 	return new Discord.MessageEmbed(options);
 }
 
